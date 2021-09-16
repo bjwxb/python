540f719c1990536ef6b9dfa8612faadb7768b4f5
wuxiaobo <13651269612@163.com>
2021-09-16 18:05:44 +0800

    异常/调试方法

---
+++ day5.py
@@ -0,0 +1,43 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+# 异常处理 try...except...finally...
+# debug:
+# 1. print 用print()最大的坏处是将来还得删掉它,
+# 2. assert 凡是用print()来辅助查看的地方，都可以用断言（assert）来替代
+# 3. logging 和assert比，logging不会抛出错误，而且可以输出到文件
+#    它允许你指定记录信息的级别，有debug，info，warning，error等几个级别，
+#    当我们指定level=INFO时，logging.debug就不起作用了。同理，指定level=WARNING后，
+#    debug和info就不起作用了。这样一来，你可以放心地输出不同级别的信息，也不用删除，最后统一控制输出哪个级别的信息。
+# 4. pdb
+# 5. 设置断点
+
+
+def test():
+    while 1:
+        s = input('请输入年龄：')
+        try:
+            age = int(s)
+            if age < 0:
+                print('input age wrong!')
+            elif age < 10:
+                print('>>> 是个小孩')
+            elif age < 20:
+                print('=== 是个少年')
+            elif age < 50:
+                print('---- 是个青年人')
+            elif age < 100:
+                print('>>>> 挺好的')
+            else:
+                print(">>> 福如东海 寿比南山 <<<")
+            break
+        except ValueError:  # except 可以根据不同的异常做不同的处理
+            pass
+        except Exception as e:
+            print('except:', e)
+        finally:  # 如果定义了finally 不论是否有异常，最后都会执行finally模块
+            logging.info(f'******** 肯定会执行 *******{s}')
+    pass
+
+
+if __name__ == '__main__':
+    test()
